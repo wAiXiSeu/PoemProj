cde959500c2c9f4b440e33e0dd81a7d5ed0e24ca
wAiXiSeu <yuecheng_w@126.com>
2018-01-10 16:05:53 +0800

    First Commit. PoemCrawler is used for crawling poems from website. PoemInfo is used for dealing with poems.

---
+++ PoemCrawler/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/10 15:59
+# @Author  : wAiXi
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: PyCharm Community Edition
+
